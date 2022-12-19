1dd44a08cf3f33fb543e64e1abfef65ac5a0ba43
PaulAREnriquez <paulaldrinr.enriquez@gmail.com>
2022-12-19 15:09:25 +0800

    Parameterizing command-line arguments

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,10 +1,32 @@
 import openai
 import os
+import argparse
 
 OPEN_API_KEY = "OPENAI_API_KEY"
 
 
 def main():
+    # Parameterizing arguments from the command line
+    parser = argparse.ArgumentParser(description="Pychat v0.1")
+    # max-tokens is the flag
+    parser.add_argument(
+        "--max-tokens", help="Maximum size of tokes used", type=int, default=4000
+    )
+    parser.add_argument(
+        "--engine",
+        help="The openai engine to use",
+        type=str,
+        default="text-davinci-003",
+    )
+
+    args = parser.parse_args()
+    # max-tokens become max_tokens here
+    max_tokens = args.max_tokens
+    engine = args.engine
+
+    print("Options:")
+    print(f"Max tokens: {max_tokens}")
+    print(f"Engine: {engine}")
 
     open_ai_api_key = os.getenv(OPEN_API_KEY)
 
@@ -12,13 +34,10 @@ def main():
         print("OPENAI_API_KEY required")
         exit(-1)
 
-    print("Pychat v0.1")
+    # print("Pychat v0.1")
 
     query = input("Input: ")
     if query != "":
-
-        max_tokens = 4000
-        engine = "text-davinci-003"
 
         completion = openai.Completion.create(
             engine=engine, prompt=query, max_tokens=max_tokens
