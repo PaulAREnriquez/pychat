cd343815f8b191919a8b3c72df14be4d4966fb2b
PaulAREnriquez <paulaldrinr.enriquez@gmail.com>
2022-12-19 13:55:02 +0800

    Resolves #2, #3

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,12 +1,12 @@
 import openai
 import os
 
-OPEN_API_KEY = 'OPENAI_API_KEY'
+OPEN_API_KEY = "OPENAI_API_KEY"
 
 # todo
 # handle detection of OPEN_API_KEY availability
 # max_tokens can be set as a parameter outside with a default value
-# 
+#
 
 
 def main():
@@ -14,16 +14,17 @@ def main():
     open_ai_api_key = os.getenv(OPEN_API_KEY)
 
     if open_ai_api_key == None:
-        print('OPENAI_API_KEY required')
+        print("OPENAI_API_KEY required")
         exit(-1)
 
     print("Pychat v0.1")
     query = input("Input: ")
 
     max_tokens = 4000
+    engine = "text-davinci-003"
 
     completion = openai.Completion.create(
-        engine="text-davinci-003", prompt=query, max_tokens = max_tokens
+        engine=engine, prompt=query, max_tokens=max_tokens
     )
 
     output = completion.choices[0].text
